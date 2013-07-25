34e0403bce77126bb5df0de392fb6aea726fc0a4
Jaime Pillora <jpillora@gmail.com>
2013-07-25 12:02:59 +1000

    init

---
+++ test/basic-test.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect;
+var module = require("../");
+
+describe("some basic tests", function() {
+
+  it("simple", function() {
+    expect(1).to.equal(1);
+  });
+
+});
